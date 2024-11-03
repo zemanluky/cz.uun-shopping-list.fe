e56fed35778f61e8a0255de1a45a67ebf55155dc
zemanluky <zemanluky@gmail.com>
2024-11-04 00:13:15 +0100

    ✨ Add feature to show incomplete items only

--- src/components/features/ShoppingList/ShoppingListItems.tsx
+++ src/components/features/ShoppingList/ShoppingListItems.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import {Heading} from "@ParkComponents/ui";
-import {Box, BoxProps, VStack} from "../../../../styled-system/jsx";
+import React, {useMemo, useState} from "react";
+import {Checkbox, Heading} from "@ParkComponents/ui";
+import {Box, BoxProps, HStack, VStack} from "../../../../styled-system/jsx";
 import {ShoppingListItem as ShoppingListItemType} from "../../../types/shopping-list.ts";
 import {ShoppingListItem} from "@Components/features/ShoppingList/ShoppingListItem.tsx";
 
@@ -9,13 +9,22 @@ interface ShoppingListItemsProps extends BoxProps {
 }
 
 export const ShoppingListItems: React.FC<ShoppingListItemsProps> = ({items, ...boxProps}) => {
-
+    const [showIncompleteOnly, setShowIncompleteOnly] = useState<boolean>(false);
+    const filteredItems = useMemo(() => items.filter(item => {
+        if (!showIncompleteOnly) return item;
+        return item.completed_at === null;
+    }), [items, showIncompleteOnly]);
 
     return <Box {...boxProps}>
-        <Heading as={'h3'} fontSize={'2xl'} fontWeight={'bold'} display={'block'} mb={'4'}>Položky</Heading>
+        <HStack justifyContent={'space-between'} mb={'4'}>
+            <Heading as={'h3'} fontSize={'2xl'} fontWeight={'bold'} display={'block'}>Položky</Heading>
+            <Checkbox checked={!showIncompleteOnly} onCheckedChange={() => setShowIncompleteOnly(!showIncompleteOnly)}>
+                Zobrazit hotové
+            </Checkbox>
+        </HStack>
         <VStack gap={2}>
             <ShoppingListItem/>
-            {items.map(item => <ShoppingListItem key={item.id} item={item} />)}
+            {filteredItems.map(item => <ShoppingListItem key={item.id} item={item} />)}
         </VStack>
     </Box>
 }
