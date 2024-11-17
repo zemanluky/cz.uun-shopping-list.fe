5bb170114df52b9c22bed2af9e9089b76d9e4ae3
zemanluky <zemanluky@gmail.com>
2024-11-17 15:16:46 +0100

    🐛 Fix no bottom padding on the page

--- src/components/layout/PageWrapper.tsx
+++ src/components/layout/PageWrapper.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import {Navbar} from "@Components/layout/Navbar.tsx";
 import { Outlet } from "react-router-dom";
+import {Box} from "../../../styled-system/jsx";
 
 export const PageWrapper: React.FC = () => {
-    return <>
+    return <Box pb='8'>
         <Navbar/>
         <Outlet/>
-    </>;
+    </Box>;
 }
